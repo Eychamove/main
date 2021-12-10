bc09592a02599eb535d93601206eb50ff1004203
AlwinFassbender <alwin.fassbender@gmail.com>
2021-12-10 02:32:50 +0100

    endlich auch mal mit dem zocken fertig lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bioprocess_lexicon/quiz_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bioprocess_lexicon/alphabet_scroll_page.dart';
@@ -14,45 +15,75 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bioprocess Lexicon',
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: BioprocessApp(),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const BioprocessApp(),
     );
   }
 }
 
-class BioprocessApp extends StatefulWidget{
+class BioprocessApp extends StatefulWidget {
+  const BioprocessApp({Key? key}) : super(key: key);
+
   @override
   _BioprocessLex createState() => _BioprocessLex();
 }
 
-class _BioprocessLex extends State<BioprocessApp>{
+class _BioprocessLex extends State<BioprocessApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) {
+    List<Widget> _widgetOptions = [
+      AlphabetScrollPage(
+        onClickedItem: (item) {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => LexEintragPage(lexikonEintrag: item)));
+        },
+        items: LexikonEintrag.lexikonEintraege,
+      ),
+      const CooleQuizPage(),
+    ];
+
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('BIOPROCESSING', style: TextStyle(fontWeight: FontWeight.bold)),
+        title: const Text('BIOPROCESSING',
+            style: TextStyle(fontWeight: FontWeight.bold)),
         titleTextStyle: const TextStyle(fontSize: 25),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: () {
-            showSearch(context: context, delegate: DataSearch());
-          })
+          _selectedIndex == 0
+              ? IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    showSearch(context: context, delegate: DataSearch());
+                  })
+              : Container(),
         ],
       ),
-      body: AlphabetScrollPage(
-        onClickedItem: (item) {
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => LexEintrag(titel: item))
-          );
-        },
-        items: LexiList.obj,
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.quiz),
+            label: 'Business',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
-  );
+    );
+  }
 }
-
-
-
-
-
---
+++ lib/quiz_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CooleQuizPage extends StatefulWidget {
+  const CooleQuizPage({Key? key}) : super(key: key);
+
+  @override
+  _CooleQuizPageState createState() => _CooleQuizPageState();
+}
+
+class _CooleQuizPageState extends State<CooleQuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Quiz hier"),
+    );
+  }
+}
